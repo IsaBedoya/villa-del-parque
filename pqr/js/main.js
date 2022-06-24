c27eb89affef3d73ac68f5e21071b88adae5f1ba
Isa Bedoya <isabelabedoyagaviria@gmail.com>
2022-06-24 13:06:06 -0500

    villaDelParque

---
+++ pqr/js/main.js
@@ -0,0 +1,12 @@
+// const pqr = document.querySelector('#form');
+// const mailTo = document.querySelector('#trucazo');
+
+// pqr.addEventListener('submit', handleSubmit);
+
+// function handleSubmit(event){
+//     event.preventDefault();
+//     const form = new FormData(this);
+//     mailTo.setAttribute('href', `mailto:isbega0406@gmail.com?subject=Nombre: ${form.get('name')}, Mensaje: ${form.get('asunto')}&body=${form.get('message')}.
+//     Contacto: ${form.get('phoneNumber')} o ${form.get('email')}`);
+//     mailTo.click();
+// }
