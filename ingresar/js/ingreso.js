f70e69d6ac800cb49f27eb24d764eb374886a4b7
Isa Bedoya <isabelabedoyagaviria@gmail.com>
2022-06-25 09:27:22 -0500

    actualization

--- ingresar/js/ingreso.js
+++ ingresar/js/ingreso.js
@@ -1,1 +1,11 @@
-const btnSignIn = document.getElementById
+const $btnSignIn= document.querySelector('.signInBtn'),
+      $btnSignUp = document.querySelector('.signUpBtn'),  
+      $signUp = document.querySelector('.sign-up'),
+      $signIn  = document.querySelector('.sign-in');
+
+document.addEventListener('click', e => {
+    if (e.target === $btnSignIn || e.target === $btnSignUp) {
+        $signIn.classList.toggle('active');
+        $signUp.classList.toggle('active');
+    }
+});
