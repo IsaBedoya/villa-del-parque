afd8fb42b4018cfacd62575a17451eba0fa52278
Isa Bedoya <isabelabedoyagaviria@gmail.com>
2022-09-02 12:23:00 -0500

    mi cuenta actualizacion

--- miCuenta/js/miCuenta.js
+++ miCuenta/js/miCuenta.js
@@ -4,3 +4,11 @@ const menu = document.querySelector('#menu');
 btnMenu.addEventListener('click', () => {
     menu.classList.toggle('active');
 });
+
+
+const btnVerMas = document.getElementById('btnVerMas');
+
+btnVerMas.addEventListener('click', (e) => {
+    e.preventDefault();
+    Swal.fire('No hay más facturas');
+})
